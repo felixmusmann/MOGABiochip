bb7e0338859ae6ba064a73c3c4de203aab99d7b2
Felix Musmann <fmusmann@gmail.com>
2016-06-14 16:28:43 +0200

    exit if compilation fails

--- Run.sh
+++ Run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 inputPath="data/input"
 graphsPath="$inputPath/graphs"
 libFile="$inputPath/TS_lib.txt"
@@ -10,10 +12,18 @@ minWidth=5
 minHeight=5
 
 echo "Compiling source files."
-javac -encoding utf8 -classpath lib/gson-2.6.2.jar:lib/jmetal-algorithm-5.0.jar:lib/jmetal-core-5.0.jar:lib/jmetal-exec-5.0.jar:lib/jmetal-problem-5.0.jar src/compilation/*.java src/synthesis/*.java src/synthesis/model/*.java
+if [ javac -encoding utf8 -classpath lib/gson-2.6.2.jar:lib/jmetal-algorithm-5.0.jar:lib/jmetal-core-5.0.jar:lib/jmetal-exec-5.0.jar:lib/jmetal-problem-5.0.jar src/compilation/*.java src/synthesis/*.java src/synthesis/model/*.java ]
+then
+  echo "Compilation successful."
+else
+  echo "Compilation failed."
+  exit
+fi
 
 echo "Start synthesis."
-for graph in $graphsPath/*; do
-  echo $graph
-  java -classpath src:lib/gson-2.6.2.jar:lib/jmetal-algorithm-5.0.jar:lib/jmetal-core-5.0.jar:lib/jmetal-exec-5.0.jar:lib/jmetal-problem-5.0.jar synthesis.NSGARunner $iterations $population $mutationRate $minWidth $minHeight $graph $libFile $devicesFile
+for graph in ${graphsPath}/*; do
+  echo ${graph}
+  java -classpath src:lib/gson-2.6.2.jar:lib/jmetal-algorithm-5.0.jar:lib/jmetal-core-5.0.jar:lib/jmetal-exec-5.0.jar:lib/jmetal-problem-5.0.jar synthesis.NSGARunner ${iterations} ${population} ${mutationRate} ${minWidth} ${minHeight} ${graph} ${libFile} ${devicesFile}
 done
+
+echo "Ending script."
