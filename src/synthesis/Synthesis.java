abd7b0b5e3302651040b6d8ef21bb9e69ccccdc3
Felix Musmann <fmusmann@gmail.com>
2016-07-17 17:24:28 +0200

    Fix

--- src/synthesis/Synthesis.java
+++ src/synthesis/Synthesis.java
@@ -5,6 +5,7 @@ import compilation.DirectedGraph;
 import synthesis.model.Biochip;
 import synthesis.model.Device;
 import synthesis.model.DeviceLibrary;
+import synthesis.model.Shape;
 
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -18,7 +19,7 @@ public class Synthesis {
 
     public static void main(String[] args) {
     	// Input files
-    	String appName = "interp_dil";
+    	String appName = "ivd";
     	String appFile = "data/input/graphs/" + appName + ".txt";
     	String libFile = "data/input/TS_lib.txt";
     	String devicesFile = "data/input/devices.json";
@@ -34,23 +35,21 @@ public class Synthesis {
     	int minRadius = 10;
     	int maxRadius = 10;
     	
-    	DeviceLibrary deviceLib = JSONParser.readDeviceLibrary(devicesFile);
-    	SynthesisProblem problem = new SynthesisProblem(minWidth, minHeight, appFile, libFile, deviceLib);
-    	BiochipSolution solution = problem.createSolution();
-    	
-    	Arch my_arch = solution.toCompileArchitecture(); 
-    	System.out.println(my_arch.toString()); 
-    	
-    	try {
-    		long cpu_start_t = System.currentTimeMillis(); 
-			double time = compilation.Main.compile(solution.toCompileArchitecture(), appFile, libFile, deadline, minWindow, maxWindow, minRadius, maxRadius);
-			long cpu_stop_t = System.currentTimeMillis(); 
-			System.out.println("CPU time = " + (cpu_stop_t - cpu_start_t) + " ms");
-			System.out.println("app completes in " +  time + " s" );
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    	//DeviceLibrary deviceLib = JSONParser.readDeviceLibrary(devicesFile);
+    	//SnthesisProblem problem = new SynthesisProblem(1, 1, minWidth, minHeight, appFile, libFile, deviceLib);
+        Biochip solution;
+        try {
+            solution = JSONParser.readBiochip("data/arch.json").get(0);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        //problem.evaluate(solution);
+
+        System.out.println(solution);
+        System.out.println(solution.toCompileArchitecture());
+        System.out.println(solution.getExecutionTime(appFile, libFile, deadline, minWindow, maxWindow, minRadius, maxRadius));
+    }
 
     public static void waitForEnter() {
         try {
